30c33ec1461f87d7c9dde49f543e9336e9907e37
Gabriel Leme <gabriel.chimenezleme@gmail.com>
2025-06-11 16:13:45 -0300

    v2

--- internal/pipeline/timestamp.go
+++ internal/pipeline/timestamp.go
@@ -14,6 +14,7 @@ var timestampPatterns = []string{
 	"2006-01-02T15:04:05.000Z07:00",
 	"2006-01-02T15:04:05.000Z",
 	"2006-01-02 15:04:05",
+	"2006-01-02 15:04:05,000",
 	"02/Jan/2006:15:04:05 -0700",
 	"Jan 2 15:04:05",
 	"01/02/2006 15:04:05",
@@ -21,7 +22,7 @@ var timestampPatterns = []string{
 	"2006-01-02 15:04:05.999999999 -0700 MST",
 }
 
-var extractRegex = regexp.MustCompile(`(?i)(\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:?\d{2}| UTC)?|\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2} [-+]\d{4}|[A-Za-z]{3} \d{1,2} \d{2}:\d{2}:\d{2}|\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2})`)
+var extractRegex = regexp.MustCompile(`(?i)(\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}:\d{2}([.,]\d+)?(Z|[+-]\d{2}:?\d{2}| UTC)?|\d{2}/[A-Za-z]{3}/\d{4}:\d{2}:\d{2}:\d{2} [-+]\d{4}|[A-Za-z]{3} \d{1,2} \d{2}:\d{2}:\d{2}|\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2})`)
 
 func TryExtractTimestamp(msg string) (time.Time, bool) {
 	match := extractRegex.FindString(msg)
